e62cab2118c570723a03eb66ab29a842a1b9e9da
Jonas Svedstrup <jonassvedstrup@gmail.com>
2025-08-13 03:58:35 +0200

    add some images

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+const mapName = "roslyn-peninsula";
+</script>
 <template>
   <main id="main-about">
     <section>
       <h1>About page</h1>
+      <img v-bind:src="`/maps/${mapName}/${mapName}-top.png`" />
     </section>
   </main>
 </template>
